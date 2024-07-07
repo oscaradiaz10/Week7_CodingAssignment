7a941eb8a8e78aa2602bcc09431fb7131b5b68c7
oscaradiaz10 <oscar.di@me.com>
2024-07-06 23:13:46 -0500

    Week 10 updates

--- src/main/java/projects/projectsApp.java
+++ src/main/java/projects/projectsApp.java
@@ -13,9 +13,13 @@ public class projectsApp {
 	private ProjectService projectService = new ProjectService();
 	private Scanner scanner = new Scanner(System.in);
 	
+	private Project curProject;
+	
 	// @formatter:off
 	private List<String> operations = List.of(
-		"1) Add a project"
+		"1) Add a project",
+		"2) List projects",
+		"3) Select a project"
 	);
 	// @formatter:on
 	
@@ -33,11 +37,15 @@ public class projectsApp {
 				case -1:
 					done = exitMenu();
 					break;
-					
 				case 1:
 					createProject();
 					break;
-					
+				case 2:
+					listProjects();
+					break;
+				case 3:
+					selectProject();
+					break;
 				default:
 					System.out.println("\n" + selection + " is not a valid selection. Try again.");
 					break;
@@ -47,6 +55,27 @@ public class projectsApp {
 				System.out.println("\nError: " + e + " Try again.");
 			}
 		}
+	}
+
+	private void selectProject() {
+		listProjects();
+		Integer projectId = getIntInput("Enter a project ID to select a project");
+		
+		curProject = null;
+		
+		curProject = projectService.fetchProjectById(projectId);
+		
+		//if(Objects.isNull(curProject)) {
+		//	System.out.println("\nInvalid project ID selected.");
+		//}
+	}
+
+	private void listProjects() {
+		List<Project> projects = projectService.fetchAllProjects();
+		
+		System.out.println("\nProjects:");
+		
+		projects.forEach(project -> System.out.println("  " + project.getProjectId() + ": " + project.getProjectName()));
 	}
 
 	private void createProject() {
@@ -98,6 +127,12 @@ public class projectsApp {
 		System.out.println("\nThese are the available selections. Press the Enter key to quit");
 		
 		operations.forEach(line -> System.out.println("  " + line));
+		
+		if(Objects.isNull(curProject)) {
+			System.out.println("\nYou are not working with a project.");
+		} else {
+			System.out.println("\nYou aren working with project: " + curProject);
+		}
 	}
 
 	private Integer getIntInput(String prompt) {

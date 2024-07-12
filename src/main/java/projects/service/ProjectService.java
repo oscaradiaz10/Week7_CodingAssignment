6fe20944829394e5193bce9ea8e5d62f6f466812
oscaradiaz10 <oscar.di@me.com>
2024-07-11 23:36:21 -0500

    Finished project

--- src/main/java/projects/service/ProjectService.java
+++ src/main/java/projects/service/ProjectService.java
@@ -2,9 +2,11 @@ package projects.service;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 import projects.dao.ProjectDao;
 import projects.entity.Project;
+import projects.exception.DbException;
 
 public class ProjectService {
 
@@ -15,11 +17,26 @@ public class ProjectService {
 	}
 
 	public List<Project> fetchAllProjects() {
-		return projectDao.fetchAllProjects();
+		return projectDao.fetchAllProjects()
+		.stream()
+		.sorted((r1, r2) -> r1.getProjectId() - r2.getProjectId())
+		.collect(Collectors.toList());
 	}
 
 	public Project fetchProjectById(Integer projectId) {
 		return projectDao.fetchProjectById(projectId).orElseThrow(() -> new NoSuchElementException(
 			"Project with project ID=" + projectId + " does not exist."));
+	}
+
+	public void modifyProjectDetails(Project project) {
+		if(!projectDao.modifyProjectDetails(project)) {
+			throw new DbException("Project with ID=" + project.getProjectId() + " does not exist");
+		}
+	}
+
+	public void deleteProject(Integer projectId) {
+		if(!projectDao.deleteProject(projectId)) {
+			throw new DbException("Project with ID=" + projectId + " does not exist");
+		}
 	}
 }
